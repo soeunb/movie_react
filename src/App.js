ddb6a442bf9a4c6755e371b79193759304ebf586
soeunb <b_soeun_@naver.com>
2022-03-07 00:04:50 +0900

    react 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+function Food({fav}){
+  return <h1>I like {fav}</h1>;
+}
+
 function App() {
-  return <div> hello </div>;
+  return (
+  <div>
+    <h1>hello</h1>
+    <Food fav="kimchi" />
+    <Food fav="11111" />
+    <Food fav="22222" />
+  </div>
+  );
 }
 
 export default App;
